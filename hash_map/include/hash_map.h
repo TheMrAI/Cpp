545c3d9b5bf4d566a500ae84e52f1b6852e7e5a4
TheMrAI <mr.istvan.acs@gmail.com>
2020-03-29 17:25:38 +0200

    Add basic types to hash_map

--- hash_map/include/hash_map.h
+++ hash_map/include/hash_map.h
@@ -1,4 +1,62 @@
 #ifndef HASH_MAP
 #define HASH_MAP
 
+#include <functional>
+#include <list>
+#include <vector>
+
+namespace mrai
+{
+template <typename Key, typename Value, typename HashFunction = std::hash<Key>,
+          typename KeyEquality = std::equal_to<Key>>
+class hash_map
+{
+public:
+  explicit hash_map(size_t bucket_count = 20,
+                    const HashFunction& hash = HashFunction(),
+                    const KeyEquality& key_equality = KeyEquality());
+  // hash_map(const hash_map& rhs);
+  // hash_map(hash_map&& rhs);
+  // hash_map& operator=(const hash_map& rhs);
+  // hash_map& operator=(hash_map&& rhs);
+  //~hash_map();
+
+  // auto empty() -> bool;
+  // auto size() -> size_t;
+  // auto insert() -> std::pair<iterator, bool>;
+  // emplace
+  // auto erase() ->  size_t;
+  // auto at(const Key& key) -> Value&l
+  // auto contains(const Key& key) -> bool;
+  // auto find(const Key& key) -> iterator;
+
+  // auto bucket_count() const -> size_t;
+  // auto max_bucket_count() constexpr -> size_t;
+  // auto bucket_size(size_t bucket_index) const -> size_t;
+  // auto bucket(const Key& key) const -> size_t;
+
+  // auto load_factor() const -> float;
+  // auto max_load_factor() const -> float;
+  // auot max_load_factor(float max_load_factor) -> void;
+  // auto rehash(size_t bucket_count) -> void;
+  // auto reserve(size_t element_count) -> void;
+private:
+  using value_type = std::pair<Key, Value>;
+  using bucket = std::list<value_type>;
+
+  std::vector<bucket> data_;
+  const HashFunction hash_;
+  const KeyEquality key_equality_;
+};
+
+template <typename Key, typename Value, typename HashFunction,
+          typename KeyEquality>
+hash_map<Key, Value, HashFunction, KeyEquality>::hash_map(
+    size_t bucket_count, const HashFunction& hash,
+    const KeyEquality& key_equality)
+    : data_(bucket_count), hash_{ hash }, key_equality_{ key_equality }
+{
+}
+
+}  // namespace mrai
 #endif  // HASH_MAP
--- hash_map/tests/hash_map_unit_tests.cpp
+++ hash_map/tests/hash_map_unit_tests.cpp
@@ -2,7 +2,10 @@
 
 #include "hash_map.h"
 
+using namespace mrai;
+
 TEST(hash_map, nothing)
 {
   ASSERT_FALSE(true);
+  hash_map<int, std::string> lala;
 }
