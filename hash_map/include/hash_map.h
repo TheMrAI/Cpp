bf7ed96be0d06acdb8afbe7a970365f2f502f26f
TheMrAI <mr.istvan.acs@gmail.com>
2020-04-04 11:28:09 +0200

    Basic hash_map functions

--- hash_map/include/hash_map.h
+++ hash_map/include/hash_map.h
@@ -12,6 +12,12 @@ template <typename Key, typename Value, typename HashFunction = std::hash<Key>,
 class hash_map
 {
 public:
+  using value_type = std::pair<Key, Value>;
+
+  class iterator
+  {
+  };
+
   explicit hash_map(size_t bucket_count = 20,
                     const HashFunction& hash = HashFunction(),
                     const KeyEquality& key_equality = KeyEquality());
@@ -21,17 +27,18 @@ public:
   // hash_map& operator=(hash_map&& rhs);
   //~hash_map();
 
-  // auto empty() -> bool;
-  // auto size() -> size_t;
-  // auto insert() -> std::pair<iterator, bool>;
+  auto empty() -> bool;
+  auto size() -> size_t;
+  // auto insert(const value_type& value) -> std::pair<iterator, bool>;
+  // auto insert(value_type&& value) -> std::pair<iterator, bool>;
   // emplace
   // auto erase() ->  size_t;
   // auto at(const Key& key) -> Value&l
   // auto contains(const Key& key) -> bool;
   // auto find(const Key& key) -> iterator;
 
-  // auto bucket_count() const -> size_t;
-  // auto max_bucket_count() constexpr -> size_t;
+  auto bucket_count() const -> size_t;
+  auto max_bucket_count() -> size_t;
   // auto bucket_size(size_t bucket_index) const -> size_t;
   // auto bucket(const Key& key) const -> size_t;
 
@@ -41,10 +48,10 @@ public:
   // auto rehash(size_t bucket_count) -> void;
   // auto reserve(size_t element_count) -> void;
 private:
-  using value_type = std::pair<Key, Value>;
   using bucket = std::list<value_type>;
 
   std::vector<bucket> data_;
+  std::size_t element_count_;
   const HashFunction hash_;
   const KeyEquality key_equality_;
 };
@@ -54,8 +61,47 @@ template <typename Key, typename Value, typename HashFunction,
 hash_map<Key, Value, HashFunction, KeyEquality>::hash_map(
     size_t bucket_count, const HashFunction& hash,
     const KeyEquality& key_equality)
-    : data_(bucket_count), hash_{ hash }, key_equality_{ key_equality }
+    : data_{}, element_count_{ 0 }, hash_{ hash }, key_equality_{ key_equality }
 {
+  data_.reserve(bucket_count);
+}
+
+/*template <typename Key, typename Value, typename HashFunction,
+          typename KeyEquality>
+auto hash_map<Key, Value, HashFunction, KeyEquality>::insert(const value_type&
+value) -> std::pair<iterator, bool>
+{
+
+}*/
+
+template <typename Key, typename Value, typename HashFunction,
+          typename KeyEquality>
+auto hash_map<Key, Value, HashFunction, KeyEquality>::empty() -> bool
+{
+  return data_.empty();
+}
+
+template <typename Key, typename Value, typename HashFunction,
+          typename KeyEquality>
+auto hash_map<Key, Value, HashFunction, KeyEquality>::size() -> size_t
+{
+  return element_count_;
+}
+
+template <typename Key, typename Value, typename HashFunction,
+          typename KeyEquality>
+auto hash_map<Key, Value, HashFunction, KeyEquality>::bucket_count() const
+    -> size_t
+{
+  return data_.size();
+}
+
+template <typename Key, typename Value, typename HashFunction,
+          typename KeyEquality>
+auto hash_map<Key, Value, HashFunction, KeyEquality>::max_bucket_count()
+    -> size_t
+{
+  return data_.capacity();
 }
 
 }  // namespace mrai
--- hash_map/tests/hash_map_unit_tests.cpp
+++ hash_map/tests/hash_map_unit_tests.cpp
@@ -4,8 +4,32 @@
 
 using namespace mrai;
 
-TEST(hash_map, nothing)
+class default_constructed_hash_map: public ::testing::Test
 {
-  ASSERT_FALSE(true);
-  hash_map<int, std::string> lala;
+protected:
+  default_constructed_hash_map()
+  {
+  }
+
+  hash_map<int, std::string> map;
+};
+
+TEST_F(default_constructed_hash_map, empty_is_true)
+{
+  ASSERT_TRUE(map.empty());
+}
+
+TEST_F(default_constructed_hash_map, size_is_zero)
+{
+  ASSERT_EQ(map.size(), 0);
+}
+
+TEST_F(default_constructed_hash_map, bucket_count_is_zero)
+{
+  ASSERT_EQ(map.bucket_count(), 0);
+}
+
+TEST_F(default_constructed_hash_map, max_bucket_count_is_20)
+{
+  ASSERT_EQ(map.max_bucket_count(), 20);
 }
