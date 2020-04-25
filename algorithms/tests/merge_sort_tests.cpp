27419456cce6c86ed1951a1a6cb966857477543f
TheMrAI <mr.istvan.acs@gmail.com>
2020-04-25 22:10:00 +0200

    Added another test to merge sort
    This shows, that my in_place merge is still faulty.

--- algorithms/tests/merge_sort_tests.cpp
+++ algorithms/tests/merge_sort_tests.cpp
@@ -77,6 +77,19 @@ TEST(merge_sort, non_sorted_input_gets_sorted)
   ASSERT_EQ(output, expected);
 }
 
+TEST(merge_sort, on_30_element_random_sequence)
+{
+  auto input = std::vector<int>{ 16, 96, 33, 20, 6,  83, 64, 71, 64, 55,
+                                 8,  14, 19, 36, 25, 90, 70, 6,  1,  76,
+                                 46, 51, 93, 61, 19, 49, 13, 92, 77, 69 };
+  auto output = std::vector<int>{};
+  merge_sort(input.begin(), input.end(), std::back_inserter(output));
+  auto expected = std::vector<int>{ 1,  6,  6,  8,  13, 14, 16, 19, 19, 20,
+                                    25, 33, 36, 46, 49, 51, 55, 61, 64, 64,
+                                    69, 70, 71, 76, 77, 83, 90, 92, 93, 96 };
+  ASSERT_EQ(output, expected);
+}
+
 TEST(in_place_merge, empty_input)
 {
   auto input = std::vector<int>{};
@@ -160,5 +173,17 @@ TEST(merge_sort_in_place, 6_random_elements)
   auto input = std::vector<int>{ 7, 6, 1, 2, 8, 5 };
   merge_sort_in_place(input.begin(), input.end());
   auto expected = std::vector<int>{ 1, 2, 5, 6, 7, 8 };
+  ASSERT_EQ(input, expected);
+}
+
+TEST(merge_sort_in_place, on_30_element_random_sequence)
+{
+  auto input = std::vector<int>{ 16, 96, 33, 20, 6,  83, 64, 71, 64, 55,
+                                 8,  14, 19, 36, 25, 90, 70, 6,  1,  76,
+                                 46, 51, 93, 61, 19, 49, 13, 92, 77, 69 };
+  merge_sort_in_place(input.begin(), input.end());
+  auto expected = std::vector<int>{ 1,  6,  6,  8,  13, 14, 16, 19, 19, 20,
+                                    25, 33, 36, 46, 49, 51, 55, 61, 64, 64,
+                                    69, 70, 71, 76, 77, 83, 90, 92, 93, 96 };
   ASSERT_EQ(input, expected);
 }
