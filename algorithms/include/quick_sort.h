c58a2acd4a657b4d19da0f3e053195c6e9af9832
TheMrAI <mr.istvan.acs@gmail.com>
2020-04-25 21:14:19 +0200

    Add quicksort implementation

---
+++ algorithms/include/quick_sort.h
@@ -0,0 +1,44 @@
+#ifndef ALGORITHMS_QUICK_SORT
+#define ALGORITHMS_QUICK_SORT
+
+#include <algorithm>
+#include <iterator>
+
+namespace mrai
+{
+// quicksort_of the left and right
+// requires bidirectional iterator
+template <typename Iter>
+auto quicksort_lr(Iter begin, Iter end) -> void
+{
+  if (std::distance(begin, end) < 2)
+  {
+    return;
+  }
+
+  auto pivot = std::prev(end, 1);
+  auto smaller = begin;
+  auto larger = begin;
+
+  while (larger != pivot)
+  {
+    if (*larger > *pivot)
+    {
+      ++larger;
+    }
+    else
+    {
+      std::iter_swap(smaller, larger);
+      ++smaller;
+      ++larger;
+    }
+  }
+  std::iter_swap(smaller, pivot);
+
+  quicksort_lr(begin, smaller);
+  quicksort_lr(smaller, end);
+}
+
+}  // namespace mrai
+
+#endif  // ALGORITHMS_QUICK_SORT
--- algorithms/tests/binary_search_tests.cpp
+++ algorithms/tests/binary_search_tests.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 
+#include <vector>
+
 #include "binary_search.h"
 
 using namespace mrai;
