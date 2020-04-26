d27feb215e0653d7a8024581b7d5a4c6222c922a
TheMrAI <mr.istvan.acs@gmail.com>
2020-04-26 15:49:14 +0200

    Set up trie project

---
+++ containers/trie/include/trie.h
@@ -0,0 +1,11 @@
+#ifndef CONTAINERS_TRIE
+#define CONTAINERS_TRIE
+
+namespace mrai
+{
+class trie
+{
+};
+
+}  // namespace mrai
+#endif  // CONTAINERS_TRIE
---
+++ containers/trie/tests/trie_tests.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+
+#include "trie.h"
+
+TEST(lla, kk)
+{
+  ASSERT_FALSE(true);
+}
