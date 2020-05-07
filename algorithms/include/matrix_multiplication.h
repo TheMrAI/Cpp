6cd4851b9b92b47248a250fbea0eebc39c6a37b8
TheMrAI <mr.istvan.acs@gmail.com>
2020-05-07 22:42:37 +0200

    Add Matrix class
    This way I can use it a little easier, while still being able to access
    raw memory.

--- algorithms/include/matrix_multiplication.h
+++ algorithms/include/matrix_multiplication.h
@@ -7,23 +7,75 @@
 namespace mrai
 {
 template <typename T>
-auto allocate_matrix(size_t width, size_t height) -> std::unique_ptr<T[]>
-{
-  auto continuous_memory = std::make_unique<T[]>(width * height);
-
-  return continuous_memory;
-}
+using matrix_data = std::pair<std::unique_ptr<T*[]>, std::unique_ptr<T[]>>;
 
 template <typename T>
-auto setup_matrix_pointers(std::unique_ptr<T[]>& matrix, size_t width,
-                           size_t height) -> std::unique_ptr<T* []> {
+auto allocate_matrix(size_t width, size_t height) -> matrix_data<T>
+{
+  auto continuous_memory = std::make_unique<T[]>(width * height);
   auto matrix_pointer = std::make_unique<T*[]>(height);
 
   for (size_t row = 0; row < height; ++row)
   {
-    matrix_pointer[row] = &matrix[row * width];
+    matrix_pointer[row] = &continuous_memory[row * width];
   }
-  return matrix_pointer;
+  return std::make_pair(std::move(matrix_pointer),
+                        std::move(continuous_memory));
+}
+
+template <typename T>
+class Matrix
+{
+public:
+  Matrix(size_t width, size_t height);
+  ~Matrix() = default;
+
+  Matrix(Matrix& rhs) = delete;
+  Matrix(Matrix&& rhs) noexcept = default;
+  auto operator=(const Matrix& rhs) -> Matrix& = default;
+  auto operator=(Matrix&& rhs) noexcept -> Matrix& = default;
+
+  [[nodiscard]] auto width() const noexcept -> size_t;
+  [[nodiscard]] auto height() const noexcept -> size_t;
+  [[nodiscard]] auto matrix() -> std::unique_ptr<T*[]>&;
+  [[nodiscard]] auto matrix_memory() -> std::unique_ptr<T[]>&;
+
+private:
+  size_t width_;
+  size_t height_;
+
+  std::unique_ptr<T*[]> matrix_;
+  std::unique_ptr<T[]> matrix_memory_;
+};
+
+template <typename T>
+Matrix<T>::Matrix(size_t width, size_t height)
+    : width_{ width }, height_{ height }
+{
+  std::tie(matrix_, matrix_memory_) = allocate_matrix<T>(width, height);
+}
+
+template <typename T>
+auto Matrix<T>::width() const noexcept -> size_t
+{
+  return width_;
+}
+
+template <typename T>
+auto Matrix<T>::height() const noexcept -> size_t
+{
+  return height_;
+}
+
+template <typename T>
+auto Matrix<T>::matrix() -> std::unique_ptr<T* []>& {
+  return matrix_;
+}
+
+template <typename T>
+auto Matrix<T>::matrix_memory() -> std::unique_ptr<T[]>&
+{
+  return matrix_memory_;
 }
 
 }  // namespace mrai
--- algorithms/tests/matrix_multiplication_tests.cpp
+++ algorithms/tests/matrix_multiplication_tests.cpp
@@ -4,9 +4,33 @@
 
 using namespace mrai;
 
-TEST(nyo, nya)
+class matrix_test: public ::testing::Test
 {
-  auto matrix_memory = allocate_matrix<int>(10, 10);
-  auto something = setup_matrix_pointers(matrix_memory, 10, 10);
-  ASSERT_TRUE(false);
+protected:
+  Matrix<int> matrix_data{ 10, 5 };
+};
+
+TEST_F(matrix_test, width_is_10)
+{
+  ASSERT_EQ(matrix_data.width(), 10);
+}
+
+TEST_F(matrix_test, height_is_5)
+{
+  ASSERT_EQ(matrix_data.height(), 5);
+}
+
+TEST_F(matrix_test, zeroed_matrix_memory_display_zero_for_all_elements)
+{
+  memset(matrix_data.matrix_memory().get(), 0,
+         matrix_data.width() * matrix_data.height());
+
+  auto& matrix = matrix_data.matrix();
+  for (size_t i = 0; i < matrix_data.height(); ++i)
+  {
+    for (size_t j = 0; j < matrix_data.width(); ++j)
+    {
+      EXPECT_EQ(matrix[i][j], 0);
+    }
+  }
 }
