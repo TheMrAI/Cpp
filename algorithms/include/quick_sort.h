622cc7d21855194285148e9408b623782e283ea7
István Davorin Ács <mr.istvan.acs@gmail.com>
2020-12-10 12:48:03 +0100

    Implement quicksort_random

--- algorithms/include/quick_sort.h
+++ algorithms/include/quick_sort.h
@@ -3,9 +3,11 @@
 
 #include <algorithm>
 #include <iterator>
+#include <random>
 
 namespace mrai
 {
+
 // quicksort_of the left and right
 // requires bidirectional iterator
 template <typename Iter>
@@ -22,21 +24,53 @@ auto quicksort_lr(Iter begin, Iter end) -> void
 
   while (larger != pivot)
   {
-    if (*larger > *pivot)
-    {
-      ++larger;
-    }
-    else
+    if (*larger <= *pivot)
     {
       std::iter_swap(smaller, larger);
       ++smaller;
-      ++larger;
     }
+    ++larger;
   }
   std::iter_swap(smaller, pivot);
 
   quicksort_lr(begin, smaller);
   quicksort_lr(smaller, end);
+}
+
+template <typename Iter>
+auto quicksort_random(Iter begin, Iter end) ->void
+{
+  if (std::distance(begin, end) < 2)
+  {
+    return;
+  }
+
+  auto elements_in_range = std::distance(begin, end);
+  auto random_device = std::random_device();
+  auto generator = std::mt19937(random_device());
+  auto distribution = std::uniform_int_distribution<>(0, elements_in_range - 1);
+  auto random_step_count = distribution(generator);
+
+  auto pivot = std::next(begin, random_step_count);
+  auto last = std::prev(end, 1);
+  std::iter_swap(last, pivot);
+  pivot = last;
+  auto smaller = begin;
+  auto larger = begin;
+
+  while (larger != pivot)
+  {
+    if (*larger <= *pivot)
+    {
+      std::iter_swap(smaller, larger);
+      ++smaller;
+    }
+    ++larger;
+  }
+  std::iter_swap(smaller, pivot);
+
+  quicksort_random(begin, smaller);
+  quicksort_random(smaller, end);
 }
 
 }  // namespace mrai
--- algorithms/tests/quick_sort_tests.cpp
+++ algorithms/tests/quick_sort_tests.cpp
@@ -6,6 +6,7 @@
 
 using namespace mrai;
 
+// quicksort_lr
 TEST(quicksort_lr, empty_input)
 {
   auto input = std::vector<int>{};
@@ -67,6 +68,74 @@ TEST(quicksort_lr, on_30_element_random_sequence)
                                  8,  14, 19, 36, 25, 90, 70, 6,  1,  76,
                                  46, 51, 93, 61, 19, 49, 13, 92, 77, 69 };
   quicksort_lr(input.begin(), input.end());
+  auto expected = std::vector<int>{ 1,  6,  6,  8,  13, 14, 16, 19, 19, 20,
+                                    25, 33, 36, 46, 49, 51, 55, 61, 64, 64,
+                                    69, 70, 71, 76, 77, 83, 90, 92, 93, 96 };
+  ASSERT_EQ(input, expected);
+}
+
+// quicksort_random
+TEST(quicksort_random, empty_input)
+{
+  auto input = std::vector<int>{};
+  quicksort_random(input.begin(), input.end());
+  ASSERT_TRUE(input.empty());
+}
+
+TEST(quicksort_random, 1_element_in_input)
+{
+  auto input = std::vector<int>{ 1 };
+  quicksort_random(input.begin(), input.end());
+  auto expected = std::vector<int>{ 1 };
+  ASSERT_EQ(input, expected);
+}
+
+TEST(quicksort_random, 2_elements_in_order)
+{
+  auto input = std::vector<int>{ 1, 2 };
+  quicksort_random(input.begin(), input.end());
+  auto expected = std::vector<int>{ 1, 2 };
+  ASSERT_EQ(input, expected);
+}
+
+TEST(quicksort_random, 2_elements_out_of_order)
+{
+  auto input = std::vector<int>{ 2, 1 };
+  quicksort_random(input.begin(), input.end());
+  auto expected = std::vector<int>{ 1, 2 };
+  ASSERT_EQ(input, expected);
+}
+
+TEST(quicksort_random, 4_elements_in_order)
+{
+  auto input = std::vector<int>{ 1, 2, 3, 4 };
+  quicksort_random(input.begin(), input.end());
+  auto expected = std::vector<int>{ 1, 2, 3, 4 };
+  ASSERT_EQ(input, expected);
+}
+
+TEST(quicksort_random, 4_elements_out_of_order)
+{
+  auto input = std::vector<int>{ 2, 1, 4, 3 };
+  quicksort_random(input.begin(), input.end());
+  auto expected = std::vector<int>{ 1, 2, 3, 4 };
+  ASSERT_EQ(input, expected);
+}
+
+TEST(quicksort_random, 6_elements_out_of_order)
+{
+  auto input = std::vector<int>{ 7, 6, 1, 2, 8, 5 };
+  quicksort_random(input.begin(), input.end());
+  auto expected = std::vector<int>{ 1, 2, 5, 6, 7, 8 };
+  ASSERT_EQ(input, expected);
+}
+
+TEST(quicksort_random, on_30_element_random_sequence)
+{
+  auto input = std::vector<int>{ 16, 96, 33, 20, 6,  83, 64, 71, 64, 55,
+                                 8,  14, 19, 36, 25, 90, 70, 6,  1,  76,
+                                 46, 51, 93, 61, 19, 49, 13, 92, 77, 69 };
+  quicksort_random(input.begin(), input.end());
   auto expected = std::vector<int>{ 1,  6,  6,  8,  13, 14, 16, 19, 19, 20,
                                     25, 33, 36, 46, 49, 51, 55, 61, 64, 64,
                                     69, 70, 71, 76, 77, 83, 90, 92, 93, 96 };
