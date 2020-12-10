616598216ca78584e4be52762ef8053ae5aac212
István Davorin Ács <mr.istvan.acs@gmail.com>
2020-12-10 13:00:14 +0100

    Refactor quick_sort code

--- algorithms/include/quick_sort.h
+++ algorithms/include/quick_sort.h
@@ -8,16 +8,12 @@
 namespace mrai
 {
 
-// quicksort_of the left and right
-// requires bidirectional iterator
+// partition a range between begin and end
+// using the element before end as the pivot
+// begin != end otherwise the behaviour is undefined
 template <typename Iter>
-auto quicksort_lr(Iter begin, Iter end) -> void
+auto partition(Iter begin, Iter end) -> Iter
 {
-  if (std::distance(begin, end) < 2)
-  {
-    return;
-  }
-
   auto pivot = std::prev(end, 1);
   auto smaller = begin;
   auto larger = begin;
@@ -33,19 +29,36 @@ auto quicksort_lr(Iter begin, Iter end) -> void
   }
   std::iter_swap(smaller, pivot);
 
-  quicksort_lr(begin, smaller);
-  quicksort_lr(smaller, end);
+  return smaller;
 }
 
+// quicksort_of the left and right
+// requires bidirectional iterator
 template <typename Iter>
-auto quicksort_random(Iter begin, Iter end) ->void
+auto quicksort_lr(Iter begin, Iter end) -> void
 {
-  if (std::distance(begin, end) < 2)
+  auto elements_in_range = std::distance(begin, end);
+  if (elements_in_range < 2)
   {
     return;
   }
 
+  auto smaller_end = partition(begin, end);
+
+  quicksort_lr(begin, smaller_end);
+  quicksort_lr(smaller_end, end);
+}
+
+// requires bidirectional iterator
+template <typename Iter>
+auto quicksort_random(Iter begin, Iter end) ->void
+{
   auto elements_in_range = std::distance(begin, end);
+  if (elements_in_range < 2)
+  {
+    return;
+  }
+
   auto random_device = std::random_device();
   auto generator = std::mt19937(random_device());
   auto distribution = std::uniform_int_distribution<>(0, elements_in_range - 1);
@@ -54,23 +67,11 @@ auto quicksort_random(Iter begin, Iter end) ->void
   auto pivot = std::next(begin, random_step_count);
   auto last = std::prev(end, 1);
   std::iter_swap(last, pivot);
-  pivot = last;
-  auto smaller = begin;
-  auto larger = begin;
 
-  while (larger != pivot)
-  {
-    if (*larger <= *pivot)
-    {
-      std::iter_swap(smaller, larger);
-      ++smaller;
-    }
-    ++larger;
-  }
-  std::iter_swap(smaller, pivot);
+  auto smaller_end = partition(begin, end);
 
-  quicksort_random(begin, smaller);
-  quicksort_random(smaller, end);
+  quicksort_random(begin, smaller_end);
+  quicksort_random(smaller_end, end);
 }
 
 }  // namespace mrai
