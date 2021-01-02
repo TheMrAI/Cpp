a087955af38ad8d5ee77ac2db62065daa846fc67
István Davorin Ács <mr.istvan.acs@gmail.com>
2021-01-02 13:35:46 +0100

    Rename long functions

--- algorithms/include/dynamic_programming.h
+++ algorithms/include/dynamic_programming.h
@@ -8,9 +8,8 @@ namespace mrai
 {
 /* Longets common subsequence problem from Chapter 15.4 page 390, Introduction to Algorithms Third Edition by Thomas H.
  * Cormen, Charles E. Leiserson, Ronald L. Rivest, Clifford Stein */
-
-auto inline longest_common_subsequence_recursive_impl( std::string const& word_a, std::string const& word_b,
-                                                       size_t index_a, size_t index_b ) -> size_t
+auto inline lcs_recursive_impl( std::string const& word_a, std::string const& word_b, size_t index_a, size_t index_b )
+    -> size_t
 {
   if ( index_a == 0 || index_b == 0 )
   {
@@ -18,23 +17,22 @@ auto inline longest_common_subsequence_recursive_impl( std::string const& word_a
   }
   if ( word_a[index_a - 1] == word_b[index_b - 1] )
   {
-    return 1 + longest_common_subsequence_recursive_impl( word_a, word_b, index_a - 1, index_b - 1 );
+    return 1 + lcs_recursive_impl( word_a, word_b, index_a - 1, index_b - 1 );
   }
-  return std::max( longest_common_subsequence_recursive_impl( word_a, word_b, index_a, index_b - 1 ),
-                   longest_common_subsequence_recursive_impl( word_a, word_b, index_a - 1, index_b ) );
+  return std::max( lcs_recursive_impl( word_a, word_b, index_a, index_b - 1 ),
+                   lcs_recursive_impl( word_a, word_b, index_a - 1, index_b ) );
 }
 
-auto inline longest_common_subsequence_recursive( std::string const& word_a, std::string const& word_b ) -> size_t
+auto inline lcs_recursive( std::string const& word_a, std::string const& word_b ) -> size_t
 {
   if ( word_a.empty() || word_b.empty() )
   {
     return 0;
   }
-  return longest_common_subsequence_recursive_impl( word_a, word_b, word_a.size(), word_b.size() );
+  return lcs_recursive_impl( word_a, word_b, word_a.size(), word_b.size() );
 }
 
-auto inline longest_common_subsequence_dynamic_programming_bottom_up( std::string const& word_a,
-                                                                      std::string const& word_b ) -> size_t
+auto inline lcs_dp_bottom_up( std::string const& word_a, std::string const& word_b ) -> size_t
 {
   if ( word_a.empty() || word_b.empty() )
   {
@@ -59,12 +57,6 @@ auto inline longest_common_subsequence_dynamic_programming_bottom_up( std::strin
 
   return dp[word_a.size()][word_b.size()];
 }
-
-/*
-Problem: Give an algorithm that finds the longest palindromic subsequence for a given input string.
-Input: character
-Ouptut: carac
-*/
 
 }  // namespace mrai
 
--- algorithms/tests/dynamic_programming_tests.cpp
+++ algorithms/tests/dynamic_programming_tests.cpp
@@ -6,56 +6,56 @@
 
 using namespace mrai;
 
-TEST( longest_common_subsequence_recursive, empty_inputs )
+TEST( lcs_recursive, empty_inputs )
 {
-  EXPECT_EQ( longest_common_subsequence_recursive( "", "potato" ), 0 );
-  EXPECT_EQ( longest_common_subsequence_recursive( "potato", "" ), 0 );
-  EXPECT_EQ( longest_common_subsequence_recursive( "", "" ), 0 );
+  EXPECT_EQ( lcs_recursive( "", "potato" ), 0 );
+  EXPECT_EQ( lcs_recursive( "potato", "" ), 0 );
+  EXPECT_EQ( lcs_recursive( "", "" ), 0 );
 }
 
-TEST( longest_common_subsequence_recursive, no_common_characters )
+TEST( lcs_recursive, no_common_characters )
 {
-  ASSERT_EQ( longest_common_subsequence_recursive( "aaabbbbcccc", "ddddeeeeffff" ), 0 );
+  ASSERT_EQ( lcs_recursive( "aaabbbbcccc", "ddddeeeeffff" ), 0 );
 }
 
-TEST( longest_common_subsequence_recursive, one_common_character )
+TEST( lcs_recursive, one_common_character )
 {
-  ASSERT_EQ( longest_common_subsequence_recursive( "aaabbbbecccc", "ddddeeeeffff" ), 1 );
+  ASSERT_EQ( lcs_recursive( "aaabbbbecccc", "ddddeeeeffff" ), 1 );
 }
 
-TEST( longest_common_subsequence_recursive, DNA_example )
+TEST( lcs_recursive, DNA_example )
 {
-  ASSERT_EQ( longest_common_subsequence_recursive( "ACCGGTGGAAACCC", "CCTGGGAAAGTACC" ), 10 );
+  ASSERT_EQ( lcs_recursive( "ACCGGTGGAAACCC", "CCTGGGAAAGTACC" ), 10 );
 }
 
-TEST( longest_common_subsequence_recursive, palindrome )
+TEST( lcs_recursive, palindrome )
 {
-  ASSERT_EQ( longest_common_subsequence_recursive( "character", "retcarahc" ), 5 );
+  ASSERT_EQ( lcs_recursive( "character", "retcarahc" ), 5 );
 }
 
-TEST( longest_common_subsequence_dynamic_programming_bottom_up, empty_inputs )
+TEST( lcs_dp_bottom_up, empty_inputs )
 {
-  EXPECT_EQ( longest_common_subsequence_dynamic_programming_bottom_up( "", "potato" ), 0 );
-  EXPECT_EQ( longest_common_subsequence_dynamic_programming_bottom_up( "potato", "" ), 0 );
-  EXPECT_EQ( longest_common_subsequence_dynamic_programming_bottom_up( "", "" ), 0 );
+  EXPECT_EQ( lcs_dp_bottom_up( "", "potato" ), 0 );
+  EXPECT_EQ( lcs_dp_bottom_up( "potato", "" ), 0 );
+  EXPECT_EQ( lcs_dp_bottom_up( "", "" ), 0 );
 }
 
-TEST( longest_common_subsequence_dynamic_programming_bottom_up, no_common_characters )
+TEST( lcs_dp_bottom_up, no_common_characters )
 {
-  ASSERT_EQ( longest_common_subsequence_dynamic_programming_bottom_up( "aaabbbbcccc", "ddddeeeeffff" ), 0 );
+  ASSERT_EQ( lcs_dp_bottom_up( "aaabbbbcccc", "ddddeeeeffff" ), 0 );
 }
 
-TEST( longest_common_subsequence_dynamic_programming_bottom_up, one_common_character )
+TEST( lcs_dp_bottom_up, one_common_character )
 {
-  ASSERT_EQ( longest_common_subsequence_dynamic_programming_bottom_up( "aaabbbbecccc", "ddddeeeeffff" ), 1 );
+  ASSERT_EQ( lcs_dp_bottom_up( "aaabbbbecccc", "ddddeeeeffff" ), 1 );
 }
 
-TEST( longest_common_subsequence_dynamic_programming_bottom_up, DNA_example )
+TEST( lcs_dp_bottom_up, DNA_example )
 {
-  ASSERT_EQ( longest_common_subsequence_dynamic_programming_bottom_up( "ACCGGTGGAAACCC", "CCTGGGAAAGTACC" ), 10 );
+  ASSERT_EQ( lcs_dp_bottom_up( "ACCGGTGGAAACCC", "CCTGGGAAAGTACC" ), 10 );
 }
 
-TEST( longest_common_subsequence_dynamic_programming_bottom_up, palindrome )
+TEST( lcs_dp_bottom_up, palindrome )
 {
-  ASSERT_EQ( longest_common_subsequence_dynamic_programming_bottom_up( "character", "retcarahc" ), 5 );
+  ASSERT_EQ( lcs_dp_bottom_up( "character", "retcarahc" ), 5 );
 }
