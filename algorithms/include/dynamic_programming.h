ef212c7698cd185cdb9f6bc7283c79afff4230c2
István Davorin Ács <mr.istvan.acs@gmail.com>
2021-01-02 15:13:15 +0100

    Add memoized recursive implementation to lcs
    It is equivalent to the top down approach with memoization.

--- algorithms/include/dynamic_programming.h
+++ algorithms/include/dynamic_programming.h
@@ -2,12 +2,15 @@
 #define ALGORITHMS_DYNAMIC_PROGRAMMING
 
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 namespace mrai
 {
 /* Longets common subsequence problem from Chapter 15.4 page 390, Introduction to Algorithms Third Edition by Thomas H.
- * Cormen, Charles E. Leiserson, Ronald L. Rivest, Clifford Stein */
+ * Cormen, Charles E. Leiserson, Ronald L. Rivest, Clifford Stein 
+ */
+
 auto inline lcs_recursive_impl( std::string const& word_a, std::string const& word_b, size_t index_a, size_t index_b )
     -> size_t
 {
@@ -30,6 +33,38 @@ auto inline lcs_recursive( std::string const& word_a, std::string const& word_b 
     return 0;
   }
   return lcs_recursive_impl( word_a, word_b, word_a.size(), word_b.size() );
+}
+
+auto inline lcs_recursive_impl_with_memoization( std::string const& word_a, std::string const& word_b, size_t index_a,
+                                                 size_t index_b, std::vector<std::vector<int>>& memoized ) -> size_t
+{
+  if ( index_a == 0 || index_b == 0 )
+  {
+    memoized[index_a][index_b] = 0;
+    return 0;
+  }
+  if ( memoized[index_a][index_b] != -1 )
+  {
+    return memoized[index_a][index_b];
+  }
+  if ( word_a[index_a - 1] == word_b[index_b - 1] )
+  {
+    auto value = 1 + lcs_recursive_impl_with_memoization( word_a, word_b, index_a - 1, index_b - 1, memoized );
+    memoized[index_a][index_b] = value;
+    return value;
+  }
+  return std::max( lcs_recursive_impl_with_memoization( word_a, word_b, index_a, index_b - 1, memoized ),
+                   lcs_recursive_impl_with_memoization( word_a, word_b, index_a - 1, index_b, memoized ) );
+}
+
+auto inline lcs_memoized_recursive( std::string const& word_a, std::string const& word_b ) -> size_t
+{
+  if ( word_a.empty() || word_b.empty() )
+  {
+    return 0;
+  }
+  auto memoized = std::vector<std::vector<int>>( word_a.size() + 1, std::vector<int>( word_b.size() + 1, -1 ) );
+  return lcs_recursive_impl_with_memoization( word_a, word_b, word_a.size(), word_b.size(), memoized );
 }
 
 auto inline lcs_dp_bottom_up( std::string const& word_a, std::string const& word_b ) -> size_t
--- algorithms/tests/dynamic_programming_tests.cpp
+++ algorithms/tests/dynamic_programming_tests.cpp
@@ -33,6 +33,33 @@ TEST( lcs_recursive, palindrome )
   ASSERT_EQ( lcs_recursive( "character", "retcarahc" ), 5 );
 }
 
+TEST( lcs_memoized_recursive, empty_inputs )
+{
+  EXPECT_EQ( lcs_memoized_recursive( "", "potato" ), 0 );
+  EXPECT_EQ( lcs_memoized_recursive( "potato", "" ), 0 );
+  EXPECT_EQ( lcs_memoized_recursive( "", "" ), 0 );
+}
+
+TEST( lcs_memoized_recursive, no_common_characters )
+{
+  ASSERT_EQ( lcs_memoized_recursive( "aaabbbbcccc", "ddddeeeeffff" ), 0 );
+}
+
+TEST( lcs_memoized_recursive, one_common_character )
+{
+  ASSERT_EQ( lcs_memoized_recursive( "aaabbbbecccc", "ddddeeeeffff" ), 1 );
+}
+
+TEST( lcs_memoized_recursive, DNA_example )
+{
+  ASSERT_EQ( lcs_memoized_recursive( "ACCGGTGGAAACCC", "CCTGGGAAAGTACC" ), 10 );
+}
+
+TEST( lcs_memoized_recursive, palindrome )
+{
+  ASSERT_EQ( lcs_memoized_recursive( "character", "retcarahc" ), 5 );
+}
+
 TEST( lcs_dp_bottom_up, empty_inputs )
 {
   EXPECT_EQ( lcs_dp_bottom_up( "", "potato" ), 0 );
