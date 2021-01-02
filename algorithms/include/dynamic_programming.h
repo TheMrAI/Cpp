529c4b932a603f9cd811d0339cea1b05e99b074e
István Davorin Ács <mr.istvan.acs@gmail.com>
2021-01-02 13:31:33 +0100

    Add dynamic programming impl for longest common subsequence

--- algorithms/include/dynamic_programming.h
+++ algorithms/include/dynamic_programming.h
@@ -1,34 +1,64 @@
 #ifndef ALGORITHMS_DYNAMIC_PROGRAMMING
 #define ALGORITHMS_DYNAMIC_PROGRAMMING
 
+#include <string>
+#include <vector>
+
 namespace mrai
 {
+/* Longets common subsequence problem from Chapter 15.4 page 390, Introduction to Algorithms Third Edition by Thomas H.
+ * Cormen, Charles E. Leiserson, Ronald L. Rivest, Clifford Stein */
 
-/* Longets common subsequence problem from Chapter 15.4 page 390, Introduction to Algorithms Third Edition by Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest, Clifford Stein */
-
-auto longest_common_subsequence_recursive_impl(std::string const& word_a, std::string const& word_b, size_t index_a, size_t index_b)
+auto inline longest_common_subsequence_recursive_impl( std::string const& word_a, std::string const& word_b,
+                                                       size_t index_a, size_t index_b ) -> size_t
 {
-    if(index_a == 0 || index_b == 0)
-    {
-        return 0;
-    }
-    if(word_a[index_a - 1] == word_b[index_b - 1])
-    {
-        return 1 + longest_common_subsequence_recursive_impl(word_a, word_b, index_a - 1, index_b-1);
-    }
-    return std::max(longest_common_subsequence_recursive_impl(word_a, word_b, index_a, index_b-1), longest_common_subsequence_recursive_impl(word_a, word_b, index_a - 1, index_b));
+  if ( index_a == 0 || index_b == 0 )
+  {
+    return 0;
+  }
+  if ( word_a[index_a - 1] == word_b[index_b - 1] )
+  {
+    return 1 + longest_common_subsequence_recursive_impl( word_a, word_b, index_a - 1, index_b - 1 );
+  }
+  return std::max( longest_common_subsequence_recursive_impl( word_a, word_b, index_a, index_b - 1 ),
+                   longest_common_subsequence_recursive_impl( word_a, word_b, index_a - 1, index_b ) );
 }
 
-auto longest_common_subsequence_recursive(std::string const& word_a, std::string const& word_b)
+auto inline longest_common_subsequence_recursive( std::string const& word_a, std::string const& word_b ) -> size_t
 {
-    if(word_a.empty() || word_b.empty())
-    {
-        return 0;
-    }
-    return longest_common_subsequence_recursive_impl(word_a, word_b, word_a.size(), word_b.size());
+  if ( word_a.empty() || word_b.empty() )
+  {
+    return 0;
+  }
+  return longest_common_subsequence_recursive_impl( word_a, word_b, word_a.size(), word_b.size() );
 }
 
+auto inline longest_common_subsequence_dynamic_programming_bottom_up( std::string const& word_a,
+                                                                      std::string const& word_b ) -> size_t
+{
+  if ( word_a.empty() || word_b.empty() )
+  {
+    return 0;
+  }
+  auto dp = std::vector<std::vector<size_t>>( word_a.size() + 1, std::vector<size_t>( word_b.size() + 1, 0 ) );
 
+  for ( size_t i = 0; i < word_a.size(); ++i )
+  {
+    for ( size_t j = 0; j < word_b.size(); ++j )
+    {
+      if ( word_a[i] == word_b[j] )
+      {
+        dp[i + 1][j + 1] = dp[i][j] + 1;
+      }
+      else
+      {
+        dp[i + 1][j + 1] = std::max( dp[i + 1][j], dp[i][j + 1] );
+      }
+    }
+  }
+
+  return dp[word_a.size()][word_b.size()];
+}
 
 /*
 Problem: Give an algorithm that finds the longest palindromic subsequence for a given input string.
@@ -36,6 +66,6 @@ Input: character
 Ouptut: carac
 */
 
-}
+}  // namespace mrai
 
 #endif
--- algorithms/tests/dynamic_programming_tests.cpp
+++ algorithms/tests/dynamic_programming_tests.cpp
@@ -6,29 +6,56 @@
 
 using namespace mrai;
 
-TEST(longest_common_subsequence_recursive, empty_inputs)
+TEST( longest_common_subsequence_recursive, empty_inputs )
 {
-    EXPECT_EQ(longest_common_subsequence_recursive("", "potato"), 0);
-    EXPECT_EQ(longest_common_subsequence_recursive("potato", ""), 0);
-    EXPECT_EQ(longest_common_subsequence_recursive("", ""), 0);
+  EXPECT_EQ( longest_common_subsequence_recursive( "", "potato" ), 0 );
+  EXPECT_EQ( longest_common_subsequence_recursive( "potato", "" ), 0 );
+  EXPECT_EQ( longest_common_subsequence_recursive( "", "" ), 0 );
 }
 
-TEST(longest_common_subsequence_recursive, no_common_characters)
+TEST( longest_common_subsequence_recursive, no_common_characters )
 {
-    ASSERT_EQ(longest_common_subsequence_recursive("aaabbbbcccc", "ddddeeeeffff"), 0);
+  ASSERT_EQ( longest_common_subsequence_recursive( "aaabbbbcccc", "ddddeeeeffff" ), 0 );
 }
 
-TEST(longest_common_subsequence_recursive, one_common_character)
+TEST( longest_common_subsequence_recursive, one_common_character )
 {
-    ASSERT_EQ(longest_common_subsequence_recursive("aaabbbbecccc", "ddddeeeeffff"), 1);
+  ASSERT_EQ( longest_common_subsequence_recursive( "aaabbbbecccc", "ddddeeeeffff" ), 1 );
 }
 
-TEST(longest_common_subsequence_recursive, DNA_example)
+TEST( longest_common_subsequence_recursive, DNA_example )
 {
-    ASSERT_EQ(longest_common_subsequence_recursive("ACCGGTGGAAACCC", "CCTGGGAAAGTACC"), 10);
+  ASSERT_EQ( longest_common_subsequence_recursive( "ACCGGTGGAAACCC", "CCTGGGAAAGTACC" ), 10 );
 }
 
-TEST(longest_common_subsequence_recursive, palindrome)
+TEST( longest_common_subsequence_recursive, palindrome )
 {
-    ASSERT_EQ(longest_common_subsequence_recursive("character", "retcarahc"), 5);
+  ASSERT_EQ( longest_common_subsequence_recursive( "character", "retcarahc" ), 5 );
+}
+
+TEST( longest_common_subsequence_dynamic_programming_bottom_up, empty_inputs )
+{
+  EXPECT_EQ( longest_common_subsequence_dynamic_programming_bottom_up( "", "potato" ), 0 );
+  EXPECT_EQ( longest_common_subsequence_dynamic_programming_bottom_up( "potato", "" ), 0 );
+  EXPECT_EQ( longest_common_subsequence_dynamic_programming_bottom_up( "", "" ), 0 );
+}
+
+TEST( longest_common_subsequence_dynamic_programming_bottom_up, no_common_characters )
+{
+  ASSERT_EQ( longest_common_subsequence_dynamic_programming_bottom_up( "aaabbbbcccc", "ddddeeeeffff" ), 0 );
+}
+
+TEST( longest_common_subsequence_dynamic_programming_bottom_up, one_common_character )
+{
+  ASSERT_EQ( longest_common_subsequence_dynamic_programming_bottom_up( "aaabbbbecccc", "ddddeeeeffff" ), 1 );
+}
+
+TEST( longest_common_subsequence_dynamic_programming_bottom_up, DNA_example )
+{
+  ASSERT_EQ( longest_common_subsequence_dynamic_programming_bottom_up( "ACCGGTGGAAACCC", "CCTGGGAAAGTACC" ), 10 );
+}
+
+TEST( longest_common_subsequence_dynamic_programming_bottom_up, palindrome )
+{
+  ASSERT_EQ( longest_common_subsequence_dynamic_programming_bottom_up( "character", "retcarahc" ), 5 );
 }
