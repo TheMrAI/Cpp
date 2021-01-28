3f2429276d1348a2a9fb035c4131c0284b05d6d4
István Davorin Ács <mr.istvan.acs@gmail.com>
2021-01-28 12:52:42 +0100

    Code Jam 2020 Round1_C overrandomized solution

--- code_jam/2020/round_1C/overexcited_fan.cpp
+++ code_jam/2020/round_1C/overexcited_fan.cpp
@@ -1,48 +1,52 @@
 #include <iostream>
-#include <vector>
 #include <limits>
+#include <vector>
 
-auto catch_pepurr(int x, int y, std::string const& route) -> std::string
+// https://codingcompetitions.withgoogle.com/codejam/round/000000000019fef4/0000000000317409
+
+auto catch_pepurr( int x, int y, std::string const& route ) -> std::string
 {
-    auto route_locations = std::vector<std::pair<int, int>>{};
-    route_locations.emplace_back(x, y);
-    for(auto character : route)
+  auto route_locations = std::vector<std::pair<int, int>>{};
+  route_locations.emplace_back( x, y );
+  for ( auto character : route )
+  {
+    switch ( character )
     {
-        switch(character)
-        {
-            case 'N':
-                ++y;
-            break;
-            case 'S':
-                --y;
-            break;
-            case 'W':
-                --x;
-            break;
-            case 'E':
-                ++x;
-            break;
-        }
-        route_locations.emplace_back(x, y);
+      case 'N':
+        ++y;
+        break;
+      case 'S':
+        --y;
+        break;
+      case 'W':
+        --x;
+        break;
+      case 'E':
+        ++x;
+        break;
+      default:
+        throw std::invalid_argument( std::string( "Unrecognized direction: " ) + character );
     }
+    route_locations.emplace_back( x, y );
+  }
 
-    auto minimum_distance = std::numeric_limits<size_t>::max();
-    for(auto i = static_cast<size_t>(1); i < route_locations.size(); ++i)
+  auto minimum_distance = std::numeric_limits<int>::max();
+  for ( auto i = 1; i < static_cast<int>( route_locations.size() ); ++i )
+  {
+    auto delta_x = std::abs( route_locations[i].first );
+    auto delta_y = std::abs( route_locations[i].second );
+    auto hamming_distance = delta_x + delta_y;
+    if ( hamming_distance <= i )
     {
-        auto delta_x = std::abs(route_locations[i].first);
-        auto delta_y = std::abs(route_locations[i].second);
-        auto hamming_distance = static_cast<size_t>(delta_x + delta_y);
-        if(hamming_distance <= i)
-        {
-            minimum_distance = std::min(minimum_distance, i);
-        }
+      minimum_distance = std::min( minimum_distance, i );
     }
+  }
 
-    if(minimum_distance == std::numeric_limits<size_t>::max())
-    {
-        return "IMPOSSIBLE";
-    }
-    return std::to_string(minimum_distance);
+  if ( minimum_distance == std::numeric_limits<int>::max() )
+  {
+    return "IMPOSSIBLE";
+  }
+  return std::to_string( minimum_distance );
 }
 
 auto main() -> int
@@ -56,7 +60,7 @@ auto main() -> int
     auto pepurr_route = std::string();
     std::cin >> x >> y;
     std::cin >> pepurr_route;
-    std::cout << "Case #" << i << ": " << catch_pepurr(x, y, pepurr_route) << std::endl;
+    std::cout << "Case #" << i << ": " << catch_pepurr( x, y, pepurr_route ) << std::endl;
   }
   return 0;
 }
--- code_jam/2020/round_1C/overrandomized.cpp
+++ code_jam/2020/round_1C/overrandomized.cpp
@@ -4,8 +4,57 @@
 #include <unordered_set>
 #include <vector>
 
+// https://codingcompetitions.withgoogle.com/codejam/round/000000000019fef4/00000000003179a1
+
 constexpr auto test_points = 10'000;
 constexpr auto code_table_length = 10;
+
+auto decode_using_benford_distribution() -> std::string
+{
+  auto frequencies = std::unordered_map<char, int>{};
+  auto distinct_codes = std::unordered_set<char>{};
+  auto element_count = test_points;
+  while ( element_count > 0 )
+  {
+    auto Qi = static_cast<uint64_t>( 0 );
+    auto code = std::string();
+    std::cin >> Qi >> code;
+
+    auto entry = frequencies.find( code[0] );
+    if ( entry == frequencies.end() )
+    {
+      entry = frequencies.emplace( code[0], 0 ).first;
+    }
+    entry->second += 1;
+
+    if ( distinct_codes.size() < code_table_length )
+    {
+      for ( auto character : code )
+      {
+        distinct_codes.insert( character );
+      }
+    }
+    --element_count;
+  }
+
+  auto benford_distribution = std::vector<std::pair<char, int>>{};
+  for ( auto const& frequency : frequencies )
+  {
+    benford_distribution.emplace_back( frequency );
+  }
+  std::sort( benford_distribution.begin(), benford_distribution.end(), []( auto const& lhs, auto const& rhs ) {
+    return lhs.second > rhs.second;
+  } );
+
+  auto code_table = std::string();
+  for ( auto code_key : benford_distribution )
+  {
+    code_table += code_key.first;
+    distinct_codes.erase( code_key.first );
+  }
+
+  return *distinct_codes.begin() + code_table;
+}
 
 auto main() -> int
 {
@@ -16,49 +65,7 @@ auto main() -> int
     auto U = 0;
     std::cin >> U;
 
-    auto frequencies = std::unordered_map<char, int>{};
-    auto distinct_codes = std::unordered_set<char>{};
-    auto element_count = test_points;
-    while ( element_count > 0 )
-    {
-      auto Qi = 0;
-      auto code = std::string();
-      std::cin >> Qi >> code;
-      
-      auto entry = frequencies.find( code[0] );
-      if ( entry == frequencies.end() )
-      {
-        entry = frequencies.emplace( code[0], 0 ).first;
-      }
-      entry->second += 1;
-
-      if ( distinct_codes.size() < code_table_length )
-      {
-        for ( auto character : code )
-        {
-          distinct_codes.insert( character );
-        }
-      }
-      --element_count;
-    }
-
-    auto benford_distribution = std::vector<std::pair<char, int>>{};
-    for ( auto const& frequency : frequencies )
-    {
-      benford_distribution.emplace_back( frequency );
-    }
-    std::sort( benford_distribution.begin(), benford_distribution.end(), []( auto const& lhs, auto const& rhs ) {
-      return lhs.second > rhs.second;
-    } );
-
-    auto code_table = std::string();
-    for ( auto code_key : benford_distribution )
-    {
-      code_table += code_key.first;
-      distinct_codes.erase( code_key.first );
-    }
-
-    std::cout << "Case #" << i << ": " << *distinct_codes.begin() << code_table << std::endl;
+    std::cout << "Case #" << i << ": " << decode_using_benford_distribution() << std::endl;
   }
   return 0;
 }
